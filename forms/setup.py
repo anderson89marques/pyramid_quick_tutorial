4897f459dc2b0970ea40c3d5b344dc82fdc58a3b
anderson marques <andersonoanjo18@gmail.com>
2018-06-28 02:40:47 -0300

    18: Forms and Validation with Deform

---
+++ forms/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+requires = [
+    'deform',
+    'pyramid',
+    'pyramid_jinja2',
+    'waitress',
+    'webtest',
+    'pyramid_debugtoolbar',
+    'pytest',
+]
+
+setup(name='tutorial',
+      install_requires=requires,
+      entry_points={
+        'paste.app_factory': [
+            'main = tutorial:main',
+        ],
+    },
+)
