97dae32d1d74847ef4da36db9c7a22267f1d6d68
anderson marques <andersonoanjo18@gmail.com>
2018-06-27 22:33:19 -0300

    10: Handling Web Requests and Responses

---
+++ request_response/tutorial/__init__.py
@@ -0,0 +1,11 @@
+from pyramid.config import Configurator
+from pyramid.response import Response
+
+
+def main(global_config, **settings):
+    config = Configurator(settings=settings)
+    config.include('pyramid_jinja2')
+    config.add_route('home', '/')
+    config.add_route('plain', '/plain')
+    config.scan('.views')
+    return config.make_wsgi_app()
